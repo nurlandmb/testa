8f34e57e3ff09204b1c54724389ff65ea70b6291
Nurlan Zhumakhan <nzt.dmb@gmail.com>
2022-08-12 14:29:12 +0600

    feat: add firebase connection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-
+import firebase, { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect } from "react";
+// const db = firebase.firestore();
+const getData = async () => {
+  // console.log(firebase);
+  const querySnapshot = await getDocs(collection(db, 'leads'));
+  querySnapshot.forEach((doc) => {
+    console.log(`${doc.id} => ${doc.data()}`);
+  });
+}
 
 function App() {
+  useEffect(() => {
+    getData();
+  }, [])
+  
   return (
     <div className="App">
       Working
